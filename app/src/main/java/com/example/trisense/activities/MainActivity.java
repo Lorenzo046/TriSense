22d35b45b2825e0b64bba43f2f18d9831194db28
Lorenzo <lorenzovalva04@gmail.com>
2026-01-20 12:35:35 +0100

    feat: Configurazione Firebase

--- app/src/main/java/com/example/trisense/activities/MainActivity.java
+++ app/src/main/java/com/example/trisense/activities/MainActivity.java
@@ -2,25 +2,24 @@ package com.example.trisense.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.trisense.R;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        database = FirebaseDatabase.getInstance().getReference();
+
+        database.child("messaggio").setValue("Ciao Firebase!");
     }
 }
